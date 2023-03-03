01c4b84d97e0b677cd7e4f3857060b51f397a172
Mehdirps <mehdi.raposo77@gmail.com>
2023-03-03 10:28:59 +0100

    🎨 Search

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import { View, SafeAreaView, FlatList, Text } from 'react-native';
+import { View, SafeAreaView, FlatList } from 'react-native';
 import { useState } from 'react';
 
 import { COLORS, NFTData } from '../constants'
 import { NFTCard, FocusedStatusBar, HomeHeader } from '../components'
 
 const Home = () => {
+
+    const [nftData, setNftData] = useState(NFTData);
+
+    const handleSearch = (value) => {
+        if (!value.length) return setNftData(NFTData);
+
+        const fitleredData = NFTData.filter((item) => item.name.toLowerCase().includes(value.toLowerCase()));
+
+        if (!fitleredData.length) {
+            setNftData(NFTData);
+            return;
+        }
+        setNftData(fitleredData);
+    }
     return (
         // Zone de sécurité d'affichage selon écran
         <SafeAreaView style={{ flex: 1 }}>
@@ -15,11 +29,11 @@ const Home = () => {
                 <View style={{ zIndex: 0 }}>
                     {/* Liste d'object */}
                     <FlatList
-                        data={NFTData}
+                        data={nftData}
                         renderItem={({ item }) => <NFTCard data={item} />}
                         keyExtractor={(item) => item.id}
                         showsVerticalScrollIndicator={false}
-                        ListHeaderComponent={<HomeHeader />}
+                        ListHeaderComponent={<HomeHeader onSearch={handleSearch} />}
                     />
                 </View>
                 {/* Background page d'accueil */}
